983420caa61fa998288f4ecd2c92102130eb64c3
wmgh <wm@novel-t.ch>
2015-04-25 20:03:38 +0200

    async test

--- node/mocha/index.js
+++ node/mocha/index.js
@@ -1,7 +1,39 @@
+/* globals exports, require */
+
 exports.sanitize = function(word) {
-	return word.toLowerCase().replace(/~/g, ' ');
+  return word.toLowerCase().replace(/~/g, ' ');
 };
 
 exports.tokenize = function(sentence) {
-	return sentence.split(' ');
-}
+  return sentence.split(' ');
+};
+
+exports.info = function(callback) {
+  var https = require('https');
+  var options = {
+    host: 'api.github.com',
+    path: '/repos/wmgh/playgrounds',
+    method: 'GET',
+    headers: {
+      'User-Agent': 'sayanee'
+    }
+  };
+  var str = '';
+
+  https
+    .request(options, function(response) {
+      response.on('data', function(data) {
+        str += data;
+      });
+
+      response.on('end', function() {
+        callback(JSON.parse(str));
+      });
+
+      response.on('error', function(error) {
+        console.log(error);
+        callback();
+      });
+    })
+    .end();
+};
--- node/mocha/test/indexSpec.js
+++ node/mocha/test/indexSpec.js
@@ -1,40 +1,53 @@
-var chai = require ('chai');
+/* globals describe, it, require */
+
+// var chai = require ('chai');
 var expect = require('chai').expect;
 var word = require('../index');
 
 describe('Sanitize', function() {
 
-	// before(function() {
-	// 	console.log('before');
-	// })
+  // before(function() {
+  // 	console.log('before');
+  // })
 
-	// beforeEach(function() {
-	// 	console.log('before each');
-	// })
-	
-	// after(function() {
-	// 	console.log('after');
-	// })
+  // beforeEach(function() {
+  // 	console.log('before each');
+  // })
 
-	// afterEach(function() {
-	// 	console.log('after each');
-	// })
+  // after(function() {
+  // 	console.log('after');
+  // })
 
-	it('returns lowercase of a string', function() {
-		var inputWord = 'hello world';
-		var outputWord = word.sanitize(inputWord);
-		// chai expect
-		expect(outputWord).to.equal('hello world');
-		expect(outputWord).to.not.equal('HELLO WORLD');
-		expect(outputWord).to.be.a('string');
-		expect(outputWord).to.not.be.a('number');
-		expect(outputWord).to.contain('hello');
-	});
+  // afterEach(function() {
+  // 	console.log('after each');
+  // })
 
-	it('removes any hyphen', function() {
-		var inputWord = 'hello~world';
-		var outputWord = word.sanitize(inputWord);
+  it('returns lowercase of a string', function() {
+    var inputWord = 'hello world';
+    var outputWord = word.sanitize(inputWord);
+    // chai expect
+    expect(outputWord).to.equal('hello world');
+    expect(outputWord).to.not.equal('HELLO WORLD');
+    expect(outputWord).to.be.a('string');
+    expect(outputWord).to.not.be.a('number');
+    expect(outputWord).to.contain('hello');
+  });
 
-		expect(outputWord).to.equal('hello world');
-	});
+  it('removes any hyphen', function() {
+    var inputWord = 'hello~world';
+    var outputWord = word.sanitize(inputWord);
+
+    expect(outputWord).to.equal('hello world');
+  });
+
+  describe('Github info', function() {
+    it('returns repo info from github', function() {
+      word.info(function(reply) {
+        expect(reply.language).to.equal('JavaScript');
+        expect(reply.watchers).to.equal(157);
+        console.log('RECEIVED');
+      });
+      console.log('HELLO');
+    });
+  });
 });
