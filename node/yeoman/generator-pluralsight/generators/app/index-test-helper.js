ff5d444d53fd429f5ed5534e5d8004b5daca18b5
wmgh <styx.eu@gmail.com>
2016-01-20 16:58:38 +0100

    Lesson 4: Working with file system

--- node/yeoman/generator-pluralsight/generators/app/index-test-helper.js
+++ node/yeoman/generator-pluralsight/generators/app/index-test-helper.js
@@ -2,34 +2,34 @@
 var generators = require('yeoman-generator');
 
 var MyBase = generators.Base.extend({
-    anotherHelper: function(){
-        console.log('inside another helper');
-    }    
+  anotherHelper: function() {
+    console.log('inside another helper');
+  }
 });
 
 //module.exports = generators.Base.extend({
-    module.exports = MyBase.extend({
-    constructor: function(){
-        generators.Base.apply(this, arguments);
-        
-    },
-    
-    init: function(){
-        this.log('inside init');
-        
-        this.baz = function(){
-            this.log('inside baz');
-        }
-    },
-    
-    _foo: function () {
-        this.log('inside foo');
-    },
-    
-    bar: function () {
-        this.log('inside bar');
-        this._foo();
-        this.baz();
-        this.anotherHelper();
+module.exports = MyBase.extend({
+  constructor: function() {
+    generators.Base.apply(this, arguments);
+
+  },
+
+  init: function() {
+    this.log('inside init');
+
+    this.baz = function() {
+      this.log('inside baz');
     }
+  },
+
+  _foo: function() {
+    this.log('inside foo');
+  },
+
+  bar: function() {
+    this.log('inside bar');
+    this._foo();
+    this.baz();
+    this.anotherHelper();
+  }
 });
--- node/yeoman/generator-pluralsight/generators/app/index.js
+++ node/yeoman/generator-pluralsight/generators/app/index.js
@@ -1,31 +1,72 @@
 'use strict';
 
 var generators = require('yeoman-generator');
+var yosay = require('yosay');
 
 module.exports = generators.Base.extend({
-    constructor: function(){
-        generators.Base.apply(this, arguments);
-        this.log('constructor');
-    },
-    initializing: function(){
-      this.log('initializing');
-    },
-    prompting: function(){
-      this.log('prompting');
-    },
-    configuring: function(){
-      this.log('configuring');
-    },
-    writing: function(){
+  constructor: function() {
+    generators.Base.apply(this, arguments);
+    this.log('constructor');
+  },
+  initializing: function() {
+    this.log('initializing');
+  },
+  prompting: function() {
+    this.log('prompting');
+  },
+  configuring: function() {
+    this.log('configuring');
+  },
+  writing: {
+    init: function() {
       this.log('writing');
+      this.log('\tTemplate path: %s', this.templatePath());
+      this.log('\tDestination path: %s', this.destinationPath());
     },
-    conflicts: function(){
-      this.log('conflicts');
+    gulpFile: function() {},
+    packageJson: function() {},
+    git: function() {},
+    bower: function() {
+      // Creating JSON file from object literal
+      var bowerJson = {
+        name: 'my-app',
+        license: 'MIT',
+        dependencies: {}
+      };
+      bowerJson.dependencies['angular'] = '~1.4.6';
+      bowerJson.dependencies['lodash'] = '~3.0.1';
+      this.fs.writeJSON('bower.json', bowerJson);
     },
-    install: function(){
-      this.log('install');
+    appStaticFiles: function() {
+      this.log('\twriting app static files');
+      // For log only
+      var source = this.templatePath('_favicon.ico');
+      var destination = this.destinationPath('_favicon.ico');
+      this.log('\tsource: %s', source);
+      this.log('\tdestination: %s', destination);
+      // Copy file. No need to specify templatePath and destinationPath for copy function
+      this.copy('_favicon.ico', 'src/favicon.ico');
+      this.directory('styles', 'src/styles');
     },
-    end: function(){
-      this.log('end');
+    scripts: function() {},
+    html: function() {
+      // Copy templates and inject variables
+      this.fs.copyTpl(
+        this.templatePath('_index.html'),
+        this.destinationPath('src/index.html'), {
+          appname: 'My Cool App',
+          ngapp: 'myapp'
+        }
+      );
     }
+  },
+  conflicts: function() {
+    this.log('conflicts');
+  },
+  install: function() {
+    this.log('install');
+  },
+  end: function() {
+    console.log(yosay('this is the end'));
+  }
 });
